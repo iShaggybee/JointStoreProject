f0dbb9faca9764b88a092b57b984afea06f081d9
AreyBRSK <areybrsk@gmail.com>
2022-05-18 21:13:02 +0300

    Product cell class clean-up

--- JointStoreProject/Controllers/ProductCell.swift
+++ JointStoreProject/Controllers/ProductCell.swift
@@ -15,17 +15,6 @@ class ProductCell: UITableViewCell {
     @IBOutlet var productPriceLabel: UILabel!
     
     var addToCart: (() -> ()) = {}
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
 
     @IBAction func addToCartButtonPressed() {
         addToCart()
