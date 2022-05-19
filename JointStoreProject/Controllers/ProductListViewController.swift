aad3652a3913f8f72daef9def82f5c6a58a86a82
AreyBRSK <areybrsk@gmail.com>
2022-05-19 09:57:09 +0300

    Added search by product

--- JointStoreProject/Controllers/ProductListViewController.swift
+++ JointStoreProject/Controllers/ProductListViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ProductListViewController: UITableViewController {
-    private let products = StoreManager.shared.products.shuffled()
+    private var products = StoreManager.shared.products
     
     @IBAction func searchButtonPressed(_ sender: Any) {
         showSearchAlert()
@@ -55,7 +55,17 @@ class ProductListViewController: UITableViewController {
     
     func showSearchAlert() {
         let alert = UIAlertController(title: "Найти в каталоге", message: "", preferredStyle: .alert)
-        let searchAction = UIAlertAction(title: "Найти", style: .default)
+        let searchAction = UIAlertAction(title: "Найти", style: .default) { (alertAction) in
+            let textField = alert.textFields![0] as UITextField
+            if textField.text != "" {
+                self.products = StoreManager.shared.searchProducts(by: textField.text!)
+                DispatchQueue.main.async { self.tableView.reloadData() }
+            } else {
+                self.products = StoreManager.shared.products
+                DispatchQueue.main.async { self.tableView.reloadData() }
+            }
+        }
+            
         let cancelAction = UIAlertAction(title: "Отмена", style: .default)
         
         alert.addTextField()
